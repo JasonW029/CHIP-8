4b3c9b1dfed392ca20dcf9ca7977cbeda7cc8dbd
SirRAL <76802978+SirRAL@users.noreply.github.com>
2021-04-25 21:58:02 -0400

    Implement clear screen method and other changes to facilitate it

--- src/Display.java
+++ src/Display.java
@@ -2,20 +2,24 @@ import java.awt.*;
 import java.util.Arrays;
 import javax.swing.*;
 
+// TRUE = ON (WHITE)
+// FALSE = OFF (BLACK)
+
 public class Display extends JPanel {
 
     int scale;
     boolean[][] screen;
 
     public static void main(String[] args) {
-        //TODO: remove
         boolean[][] screenCtx = new boolean[64][32];
         for (boolean[] row : screenCtx) {
             Arrays.fill(row, true);
         }
-
-        new Display(10, screenCtx);
+        Display display = new Display(10, screenCtx);
+        // display implicitly calls paintComponent
+        display.resetScreen();
     }
+
 
     public Display(int scale, boolean[][] screen) {
         this.scale = scale;
@@ -33,14 +37,31 @@ public class Display extends JPanel {
         frame.setResizable(false);
     }
 
+    public void resetScreen() {
+        // g.clearRect(64 * this.scale, 32 * this.scale, 64 * this.scale, 32 * this.scale);
+        this.removeAll();
+
+        boolean[][] screenCtx = new boolean[64][32];
+        for (boolean[] row : screenCtx) {
+            Arrays.fill(row, false);
+        }
+
+        this.screen = screenCtx;
+
+        this.revalidate();
+        this.repaint();
+        // drawScreen();
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLACK);
 //        g.drawRect(0, 0, 200, 200);
         for (int i = 0; i < this.screen.length; i++) {
             for (int j = 0; j < this.screen[i].length; j++) {
-                if (this.screen[i][j]) {
-                    g.drawRect(i * this.scale, j * this.scale, this.scale, this.scale);
+                if (!this.screen[i][j]) {
+                    // g.drawRect(i * this.scale, j * this.scale, this.scale, this.scale);
+                    g.fillRect(i * this.scale, j * this.scale, this.scale, this.scale);
                 }
             }
         }
