390c522e6b0dd85d76665a7d377cc2af868a5f96
Ixstreme <jasonw2910@gmail.com>
2021-05-30 15:53:18 -0400

    Add more opcode unit tests

--- src/Chip8Test.java
+++ src/Chip8Test.java
@@ -24,6 +24,101 @@ public class Chip8Test {
     }
 
     @Test
+    public void test8XY0() {
+        // set register X to the value in register Y
+        short opcode = (short) 0x8AB0;
+        chip8.cpu.V[0xB] = (byte) 0b10001000;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b10001000, chip8.cpu.V[0xA]);
+    }
+
+    @Test
+    public void test8XY1() {
+        // bitwise-OR the value in register X to the value in register Y
+        short opcode = (short) 0x8AB1;
+        chip8.cpu.V[0xA] = (byte) 0b11101000;
+        chip8.cpu.V[0xB] = (byte) 0b10000011;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b11101011, chip8.cpu.V[0xA]);
+    }
+
+    @Test
+    public void test8XY2() {
+        // bitwise-AND the value in register X to the value in register Y
+        short opcode = (short) 0x8AB2;
+        chip8.cpu.V[0xA] = (byte) 0b11101000;
+        chip8.cpu.V[0xB] = (byte) 0b10000011;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b10000000, chip8.cpu.V[0xA]);
+    }
+
+    @Test
+    public void test8XY3() {
+        // bitwise-XOR the value in register X to the value in register Y
+        short opcode = (short) 0x8AB3;
+        chip8.cpu.V[0xA] = (byte) 0b11101000;
+        chip8.cpu.V[0xB] = (byte) 0b10000011;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b01101011, chip8.cpu.V[0xA]);
+    }
+
+    @Test
+    public void test8XY4_carry() {
+        // set register X to its own value PLUS the value in register Y
+        // if the value overflows 8 bits, set the carry flag in VF
+        short opcode = (short) 0x8AB4;
+        chip8.cpu.V[0xA] = (byte) 255;
+        chip8.cpu.V[0xB] = (byte) 100;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 99, chip8.cpu.V[0xA]);
+        assertEquals(1, chip8.cpu.V[0xF]);
+    }
+
+    @Test
+    public void test8XY4_no_carry() {
+        // set register X to its own value PLUS the value in register Y
+        // if the result overflows 8 bits, set the carry flag in VF
+        short opcode = (short) 0x8AB4;
+        chip8.cpu.V[0xA] = (byte) 50;
+        chip8.cpu.V[0xB] = (byte) 3;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 53, chip8.cpu.V[0xA]);
+        assertEquals(0, chip8.cpu.V[0xF]);
+    }
+
+    @Test
+    public void test8XY5_underflow() {
+        // set register X to its own value MINUS the value in register Y
+        // if the result is positive, set the carry flag in VF
+        short opcode = (short) 0x8AB5;
+        chip8.cpu.V[0xA] = (byte) 0;
+        chip8.cpu.V[0xB] = (byte) 200;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) -200, chip8.cpu.V[0xA]);
+        assertEquals(0, chip8.cpu.V[0xF]);
+    }
+
+    @Test
+    public void test8XY5_no_underflow() {
+        // set register X to its own value MINUS the value in register Y
+        // if the result is positive, set the carry flag in VF
+        short opcode = (short) 0x8AB5;
+        chip8.cpu.V[0xA] = (byte) 255;
+        chip8.cpu.V[0xB] = (byte) 200;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 55, chip8.cpu.V[0xA]);
+        assertEquals(1, chip8.cpu.V[0xF]);
+    }
+
+    @Test
+    public void test8XY6_shift_out_0() {
+    }
+
+    @Test
+    public void test8XY6_shift_out_1() {
+    }
+
+    @Test
     public void testANNN() {
         // set the index register to NNN
         short opcode = (short) 0xA598;
