bd70f36442bb51c967d28558e54010900f0d0f99
Ixstreme <78438775+Ixstreme@users.noreply.github.com>
2021-06-10 18:09:37 -0400

    Add opcode tests

--- src/Chip8Test.java
+++ src/Chip8Test.java
@@ -13,6 +13,14 @@ public class Chip8Test {
     }
 
     @Test
+    public void test6XNN() {
+        // set register X to NN
+        short opcode = 0x6AFF;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b11111111, chip8.cpu.V[0xA]);
+    }
+
+    @Test
     public void test7XNN() {
         // add NN to register X without setting register F if the sum overflows 8 bits
         short opcode = 0x7A03;
@@ -112,10 +120,70 @@ public class Chip8Test {
 
     @Test
     public void test8XY6_shift_out_0() {
+        // set register X to its own value BITSHIFTED RIGHT by 1
+        // set VF to the bit that is shifted out
+        short opcode = (short) 0x8A36;
+        chip8.cpu.V[0xA] = (byte) 0b10000010;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b01000001, chip8.cpu.V[0xA]);
+        assertEquals(0, chip8.cpu.V[0xF]);
     }
 
     @Test
     public void test8XY6_shift_out_1() {
+        // set register X to its own value BITSHIFTED RIGHT by 1
+        // set VF to the bit that is shifted out
+        short opcode = (short) 0x8A36;
+        chip8.cpu.V[0xA] = (byte) 0b10000011;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b01000001, chip8.cpu.V[0xA]);
+        assertEquals(1, chip8.cpu.V[0xF]);
+    }
+
+    @Test
+    public void test8XY7_underflow() {
+        // set register X to the value in register Y MINUS the value in register X
+        // if the result is positive, set the carry flag in VF
+        short opcode = (short) 0x8AB7;
+        chip8.cpu.V[0xA] = (byte) 200;
+        chip8.cpu.V[0xB] = (byte) 0;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) -200, chip8.cpu.V[0xA]);
+        assertEquals(0, chip8.cpu.V[0xF]);
+    }
+
+    @Test
+    public void test8XY7_no_underflow() {
+        // set register X to the value in register Y MINUS the value in register X
+        // if the result is positive, set the carry flag in VF
+        short opcode = (short) 0x8AB7;
+        chip8.cpu.V[0xA] = (byte) 200;
+        chip8.cpu.V[0xB] = (byte) 255;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 55, chip8.cpu.V[0xA]);
+        assertEquals(1, chip8.cpu.V[0xF]);
+    }
+
+    @Test
+    public void test8XYE_shift_out_0() {
+        // set register X to its own value BITSHIFTED LEFT by 1
+        // set VF to the bit that is shifted out
+        short opcode = (short) 0x8A3E;
+        chip8.cpu.V[0xA] = (byte) 0b01111111;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b11111110, chip8.cpu.V[0xA]);
+        assertEquals(0, chip8.cpu.V[0xF]);
+    }
+
+    @Test
+    public void test8XYE_shift_out_1() {
+        // set register X to its own value BITSHIFTED LEFT by 1
+        // set VF to the bit that is shifted out
+        short opcode = (short) 0x8A3E;
+        chip8.cpu.V[0xA] = (byte) 0b11111111;
+        chip8.cpu.decode(chip8, opcode);
+        assertEquals((byte) 0b11111110, chip8.cpu.V[0xA]);
+        assertEquals(1, chip8.cpu.V[0xF]);
     }
 
     @Test
